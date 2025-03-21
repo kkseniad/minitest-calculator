c7ade83848829fa0e1e49dee36433e92eaa5bbab
Kseniia Kormalova <kormalovak@gmail.com>
2025-03-21 21:34:40 +0000

    second

--- tests/test_calculator.rb
+++ tests/test_calculator.rb
@@ -1,7 +1,11 @@
-# calculator.rb
+#test/test_calculator.rb
 
-class Calculator
-  def add(a,b)
-    a+b
+require 'minitest/autorun'
+require './calculator'
+
+class TestCalculator < Minitest::Test
+  def test_addition
+    calculator = Calculator.new
+    assert_equal 4, calculator.add(2,2), "Addition method failed"
   end
 end
